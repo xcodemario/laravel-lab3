7369d59b2f90ac5cf19e5e2744623711339be6ab
Amine Baha <aminebaha115@gmail.com>
2025-04-28 12:44:14 -0400

    checked off

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,15 +12,13 @@ class PostController extends Controller
      */
     public function index()
     {
-        // eager-load the author relationship
         $posts = Post::with('author')->latest()->get();
         return view('posts.index', compact('posts'));
     }
 
-   
+
     public function show(Post $post)
     {
-        // ensure author & comments are loaded
         $post->load(['author','comments']);
         return view('posts.show', compact('post'));
     }
